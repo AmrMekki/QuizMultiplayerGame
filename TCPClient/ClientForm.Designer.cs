51771b7d9954ee54b52a29d8995e4c5dc2ee77f3
mekki21 <59305451+mekki21@users.noreply.github.com>
2021-05-09 20:20:34 +0200

    Add files via upload

---
+++ TCPClient/ClientForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace TCPClient
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnA = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 133);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server:";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(181, 130);
+            this.txtIP.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(966, 44);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "127.0.0.1:9000";
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(525, 228);
+            this.txtInfo.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInfo.Size = new System.Drawing.Size(622, 713);
+            this.txtInfo.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(569, 1005);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(468, 44);
+            this.txtMessage.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(696, 959);
+            this.label2.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(799, 1067);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(238, 65);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(556, 1067);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(238, 65);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.labelQuestion);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(43, 228);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(470, 366);
+            this.panel1.TabIndex = 19;
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Location = new System.Drawing.Point(171, 70);
+            this.labelQuestion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(154, 37);
+            this.labelQuestion.TabIndex = 13;
+            this.labelQuestion.Text = "Question:";
+            // 
+            // btnD
+            // 
+            this.btnD.BackColor = System.Drawing.Color.Cyan;
+            this.btnD.Location = new System.Drawing.Point(43, 1004);
+            this.btnD.Margin = new System.Windows.Forms.Padding(2);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(470, 130);
+            this.btnD.TabIndex = 18;
+            this.btnD.Text = "D.";
+            this.btnD.UseVisualStyleBackColor = false;
+            // 
+            // btnC
+            // 
+            this.btnC.BackColor = System.Drawing.Color.Cyan;
+            this.btnC.Location = new System.Drawing.Point(43, 870);
+            this.btnC.Margin = new System.Windows.Forms.Padding(2);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(470, 130);
+            this.btnC.TabIndex = 17;
+            this.btnC.Text = "C.";
+            this.btnC.UseVisualStyleBackColor = false;
+            // 
+            // btnB
+            // 
+            this.btnB.BackColor = System.Drawing.Color.Cyan;
+            this.btnB.Location = new System.Drawing.Point(43, 736);
+            this.btnB.Margin = new System.Windows.Forms.Padding(2);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(470, 130);
+            this.btnB.TabIndex = 16;
+            this.btnB.Text = "B. ";
+            this.btnB.UseVisualStyleBackColor = false;
+            // 
+            // btnA
+            // 
+            this.btnA.BackColor = System.Drawing.Color.Cyan;
+            this.btnA.Location = new System.Drawing.Point(43, 602);
+            this.btnA.Margin = new System.Windows.Forms.Padding(2);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(470, 130);
+            this.btnA.TabIndex = 15;
+            this.btnA.Text = "A.";
+            this.btnA.UseVisualStyleBackColor = false;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 1167);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.MinimizeBox = false;
+            this.Name = "ClientForm";
+            this.Text = "TCP/IP Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnA;
+    }
+}
+
